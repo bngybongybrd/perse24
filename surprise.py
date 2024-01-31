6c67fefc02ad830072f22669fde9be9f1f82d7d3
yx <48951196+bngybongybrd@users.noreply.github.com>
2024-01-31 13:33:48 +0700

    Update surprise.py

--- surprise.py
+++ surprise.py
@@ -1,31 +1,35 @@
 code = {
-    'A': '.-',     'B': '-...',   'C': '-.-.',  'D': '-..',
-    'E': '.',      'F': '..-.',   'G': '--.',   'H': '....',
-    'I': '..',     'J': '.---',   'K': '-.-',   'L': '.-..',
-    'M': '--',     'N': '-.',     'O': '---',   'P': '.--.',
-    'Q': '--.-',   'R': '.-.',    'S': '...',   'T': '-',
-    'U': '..-',    'V': '...-',   'W': '.--',   'X': '-..-',
-    'Y': '-.--',   'Z': '--..',
-    '0': '-----',  '1': '.----',  '2': '..---',  '3': '...--',
-    '4': '....-',  '5': '.....',  '6': '-....',  '7': '--...',
-    '8': '---..',  '9': '----.'
+    '.-': 'A',   '-...': 'B',   '-.-.': 'C',   '-..': 'D',
+    '.': 'E',    '..-.': 'F',   '--.': 'G',   '....': 'H',
+    '..': 'I',   '.---': 'J',  '-.-': 'K',   '.-..': 'L',
+    '--': 'M',   '-.': 'N',    '---': 'O',   '.--.': 'P',
+    '--.-': 'Q', '.-.': 'R',   '...': 'S',   '-': 'T',
+    '..-': 'U',  '...-': 'V',  '.--': 'W',   '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3',
+    '....-': '4', '.....': '5', '-....': '6', '--...': '7',
+    '---..': '8', '----.': '9'
 }
-text = "YOu CAN SEE How / tHis caN WoRk OuT / nOW at A look / sEe THE usE Of Key / bELOW"
-decode = ""
-caps = ""
-word_list = text.split(" / ")
+ans = ""
+crypt = ""
 
+text = "YOu CAN SEE How / tHis caN WoRk OuT / nOW at A look / sEe THE usE Of Key / bELOW"
+word_list = text.split(" / ")
 for line in word_list:
     for char in line:
         if char.isupper():
-            caps += char
-    caps += "\n"
+            crypt += "-"
+        elif char.islower():
+            crypt += "."
+        else:
+            crypt += " "
+    crypt += "|"
 
-print(caps)
-
-for line in word_list:
-    for char in line:
-        if char in code:
-            decode += code[char]
-    decode += " "
-print(decode)
+crypt = crypt.split("|")
+crypt.pop()
+for line in crypt:
+    char = line.split(" ")
+    for i in char:
+        ans += code[i]
+    ans += " "
+print(ans)
